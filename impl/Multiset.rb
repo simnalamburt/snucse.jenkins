448de5e223a730b5bd82ac369d5ee32d39682c61
Hyeon Kim <simnalamburt@gmail.com>
2015-05-04 20:34:17 +0900

    Implement multiset

--- impl/Multiset.rb
+++ impl/Multiset.rb
@@ -1,1 +1,18 @@
+class Multiset
+  def initialize(elem)
+    @data = [elem]
+  end
 
+  def contains?(e)
+    !!(@data.find_index e)
+  end
+
+  def add(e)
+    @data << e
+  end
+
+  def remove(e)
+    index = @data.find_index e
+    @data.delete_at index
+  end
+end
